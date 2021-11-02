982d0f4faff28a8bba0db430ca5b53c552103081
Perez Laiju <perezlaiju@gmail.com>
2021-11-02 03:59:47 -0400

    Initial commit

---
+++ BookStoreApi/BookStoreApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.OData.Builder;
+using System.Web.Http.OData.Extensions;
+using BookStoreApi.Models;
+
+namespace BookStoreApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Book>("Books");
+            builder.EntitySet<Category>("Categories");
+            builder.EntitySet<OrderItem>("OrderItems");
+            config.Routes.MapODataServiceRoute("odata", "odata", builder.GetEdmModel());
+        }
+    }
+}
---
+++ BookStoreApi/BookStoreApi/Controllers/CategoriesController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using System.Web.Http.OData;
+using System.Web.Http.OData.Routing;
+using BookStoreApi.Models;
+
+namespace BookStoreApi.Controllers
+{
+    /*
+    The WebApiConfig class may require additional changes to add a route for this controller. Merge these statements into the Register method of the WebApiConfig class as applicable. Note that OData URLs are case sensitive.
+
+    using System.Web.Http.OData.Builder;
+    using System.Web.Http.OData.Extensions;
+    using BookStoreApi.Models;
+    ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+    builder.EntitySet<Category>("Categories");
+    builder.EntitySet<Book>("Books"); 
+    config.Routes.MapODataServiceRoute("odata", "odata", builder.GetEdmModel());
+    */
+    public class CategoriesController : ODataController
+    {
+        private BookStoreDBEntities db = new BookStoreDBEntities();
+
+        // GET: odata/Categories
+        [EnableQuery]
+        public IQueryable<Category> GetCategories()
+        {
+            return db.Categories;
+        }
+
+        // GET: odata/Categories(5)
+        [EnableQuery]
+        public SingleResult<Category> GetCategory([FromODataUri] int key)
+        {
+            return SingleResult.Create(db.Categories.Where(category => category.Id == key));
+        }
+
+        // PUT: odata/Categories(5)
+        public IHttpActionResult Put([FromODataUri] int key, Delta<Category> patch)
+        {
+            Validate(patch.GetEntity());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Category category = db.Categories.Find(key);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            patch.Put(category);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(category);
+        }
+
+        // POST: odata/Categories
+        public IHttpActionResult Post(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Categories.Add(category);
+            db.SaveChanges();
+
+            return Created(category);
+        }
+
+        // PATCH: odata/Categories(5)
+        [AcceptVerbs("PATCH", "MERGE")]
+        public IHttpActionResult Patch([FromODataUri] int key, Delta<Category> patch)
+        {
+            Validate(patch.GetEntity());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Category category = db.Categories.Find(key);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            patch.Patch(category);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(category);
+        }
+
+        // DELETE: odata/Categories(5)
+        public IHttpActionResult Delete([FromODataUri] int key)
+        {
+            Category category = db.Categories.Find(key);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            db.Categories.Remove(category);
+            db.SaveChanges();
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // GET: odata/Categories(5)/Books
+        [EnableQuery]
+        public IQueryable<Book> GetBooks([FromODataUri] int key)
+        {
+            return db.Categories.Where(m => m.Id == key).SelectMany(m => m.Books);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CategoryExists(int key)
+        {
+            return db.Categories.Count(e => e.Id == key) > 0;
+        }
+    }
+}
