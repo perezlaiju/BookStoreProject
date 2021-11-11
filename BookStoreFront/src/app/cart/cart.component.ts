0b7640b51e9403b0a7be174bbacaa2b5ad012286
Perez Laiju <perezlaiju@gmail.com>
2021-11-11 03:31:08 -0500

    updated

--- BookStoreFront/src/app/cart/cart.component.ts
+++ BookStoreFront/src/app/cart/cart.component.ts
@@ -9,15 +9,18 @@ import { BookserviceService } from '../services/bookservice.service';
 })
 export class CartComponent implements OnInit {
   public cartBooks:Book[] | undefined;
+  public totalPrice=0;
   constructor(private BookService:BookserviceService) { }
 
   ngOnInit(): void {
     this.fillData();
+
   }
 
   fillData():void{
     this.BookService.getAllBooks().subscribe((response: any) =>{
       this.cartBooks = response.value;
+      this.cartBooks?.forEach(item=>this.totalPrice+=item.Price)
     })
   }
 
