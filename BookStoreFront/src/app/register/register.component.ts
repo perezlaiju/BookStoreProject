85c39a999bc70dc097c611fb874f90e013a1fd55
Perez Laiju <perezlaiju@gmail.com>
2021-11-11 00:33:06 -0500

    updates

--- BookStoreFront/src/app/register/register.component.ts
+++ BookStoreFront/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { AuthserviceService } from '../services/authservice.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  checkPasswords: ValidatorFn = (group: any):  ValidationErrors | null => { 
+    let pass = group.get('password').value;
+    let confirmPass = group.get('confirmPassword').value
+    return pass === confirmPass ? null : { notSame: true }
+  }
+
+  registerDetailsForm :FormGroup = new FormGroup({
+    email : new FormControl('',Validators.required),
+    password: new FormControl('', Validators.required),
+    confirmPassword : new FormControl('', Validators.required)
+  });
+
+  constructor(private auth : AuthserviceService) { }
 
   ngOnInit(): void {
+  }
+
+  handleRegister():void{
+    this.auth.register(this.registerDetailsForm.value);
   }
 
 }
